9ec57064440cec763d2f051b3fc37853d00fac1a
lema samuel <lemasam10@gmail.com>
2024-04-16 05:35:17 +0300

    assignment Done

--- main.js
+++ main.js
@@ -4,7 +4,43 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let modal = document.querySelector("#modal");
+modal.setAttribute("Class", "hidden");
 
+let heartsSpan = document.querySelectorAll("span");
+
+heartsSpan.forEach((heart) => {
+  heart.addEventListener("click", () => {
+    switchHearts(heart);
+    mimicServerCall()
+      .then((res) => {
+        if (res === "Pretend remote server notified of action!") {
+          switchHearts();
+        }
+      })
+      .catch((error) => {
+        if (error === "Random server error. Try again.") {
+          modal.className = "";
+          modal.innerHTML = error;
+          setTimeout(visibleModal, 3000);
+        }
+      });
+  });
+});
+
+function switchHearts(heart) {
+  if (heart.className === "like-glyph") {
+    heart.innerHTML = FULL_HEART;
+    heart.className = "like-glyph activated-heart";
+  } else if (heart.className === "like-glyph activated-heart") {
+    heart.innerHTML = EMPTY_HEART;
+    heart.className = "like-glyph";
+  }
+}
+
+function visibleModal() {
+  modal.className = "hidden";
+}
 
 
 //------------------------------------------------------------------------------
